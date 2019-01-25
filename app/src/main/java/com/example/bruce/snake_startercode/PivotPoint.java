d55ef195b4a83172442548fc513368f8d6e70e81
Rubberbandman2002 <lucastinouye@gmail.com>
2019-01-25 14:32:40 -0800

    done

--- app/src/main/java/com/example/bruce/snake_startercode/PivotPoint.java
+++ app/src/main/java/com/example/bruce/snake_startercode/PivotPoint.java
@@ -1,4 +1,9 @@
 package com.example.bruce.snake_startercode;
 
 public class PivotPoint {
+   private int mXCoord, mYCoord, mDegree;
+
+   public PivotPoint(int xCoord, int yCoord, int degree){
+       
+   }
 }
